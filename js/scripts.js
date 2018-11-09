067d821e0b3b14e4ec6ab927a4d31d2db2c9e4fb
Leonardo Cassani <leonardo.cassani@qualidata.com.br>
2018-11-09 12:13:33 -0200

    Ui-Router Working clean

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$( document ).ready(function() {
+var routes = angular.module('itunesSearch', ['ui.router']);
+
+routes.config(function($stateProvider){
+
     
+    var response = {
+        name: 'response',
+        url: '/testUrl',
+        templateUrl: '../views/template/response.html'
+    }
+
+    $stateProvider.state(response);
+
 });
