dd14a1bec15d426384f332e8f90b538613ee84bc
Leonardo Cassani <leonardo.cassani@qualidata.com.br>
2018-11-09 18:04:36 -0200

    doidera

--- js/scripts.js
+++ js/scripts.js
@@ -11,46 +11,27 @@ routes.config(function($stateProvider){
     
 });
 
-angular.module("itunesSearch").controller("itunesSearchCtrl", function($scope){
+angular.module("itunesSearch").controller("itunesSearchCtrl", function($scope, $http){
     $scope.loadResults = loadResults;
-   
-    _result = [
-        {musica: "Musica 1", artista: "Artista 1", album: "Album 1"},
-        {musica: "Musica 2", artista: "Artista 1", album: "Album 2"},
-        {musica: "Musica 3", artista: "Artista 1", album: "Album 3"},
-        {musica: "Musica 4", artista: "Artista 1", album: "Album 4"},
-        {musica: "Musica 5", artista: "Artista 1", album: "Album 5"},
-        {musica: "Musica 6", artista: "Artista 1", album: "Album 6"},
-        {musica: "Musica 7", artista: "Artista 1", album: "Album 7"},
-        {musica: "Musica 8", artista: "Artista 1", album: "Album 8"},
-        {musica: "Musica 9", artista: "Artista 1", album: "Album 9"},
-        {musica: "Musica 10", artista: "Artista 1", album: "Album 10"},
-        {musica: "Musica 11", artista: "Artista 1", album: "Album 11"},
-        {musica: "Musica 12", artista: "Artista 1", album: "Album 12"},
-        {musica: "Musica 13", artista: "Artista 1", album: "Album 1"},
-        {musica: "Musica 14", artista: "Artista 1", album: "Album 2"},
-        {musica: "Musica 15", artista: "Artista 1", album: "Album 3"},
-        {musica: "Musica 16", artista: "Artista 1", album: "Album 4"},
-        {musica: "Musica 17", artista: "Artista 1", album: "Album 5"},
-        {musica: "Musica 18", artista: "Artista 1", album: "Album 6"},
-        {musica: "Musica 19", artista: "Artista 1", album: "Album 7"},
-        {musica: "Musica 20", artista: "Artista 1", album: "Album 8"},
-        {musica: "Musica 21", artista: "Artista 1", album: "Album 9"},
-        {musica: "Musica 22", artista: "Artista 1", album: "Album 10"},
-        {musica: "Musica 23", artista: "Artista 1", album: "Album 11"},
-        {musica: "Musica 24", artista: "Artista 1", album: "Album 12"}
-    ];
+    $scope.pesquisa = function()
+    {
+        console.log("aaaa");
+        $http.get("http://localhost:5000/api/iTunes/search?term="+$scope.input).then(function(res){
+            
+            _result = res.data.results;
 
-    $scope.result = _result;
-    
-    var _val = ($scope.result.length % 10 ? 1 : 0)
-    var _limitTo = Math.round($scope.result.length/10+_val);
-    $scope.paginacao = [];
-    for(var _c = 0; _c < _limitTo; _c++){$scope.paginacao.push(_c+1);}
+            loadResults(0);
+            
+            var _val = ($scope.result.length % 10 ? 1 : 0)
+            var _limitTo = Math.round($scope.result.length/10+_val);
+            $scope.paginacao = [];
+            for(var _c = 0; _c < _limitTo; _c++){$scope.paginacao.push(_c+1);}
+        })
+    };
+    var _result = [];
 
     function loadResults(_pagina)
     {
-        console.log(_pagina);
         $scope.result = _result.slice(_pagina*10,_pagina*10+10);
     }
 });
